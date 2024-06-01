b3c48338fb90f3d5243861e8b85ffde0a4912f3c
SMJ1227 <112992077+SMJ1227@users.noreply.github.com>
2024-06-01 18:38:48 +0900

    enemy에서 불릿이 나감

--- WindowProgrammingTeamProject/WindowProgrammingTeamProject/main.cpp
+++ WindowProgrammingTeamProject/WindowProgrammingTeamProject/main.cpp
@@ -45,7 +45,7 @@ int map0[MAP_HEIGHT][MAP_WIDTH] = {
     {0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
     {0, 0, 2, 1, 1, 1, 1, 0, 1, 1, 3, 0},
     {0, 0, 0, 2, 1, 1, 1, 1, 1, 3, 0, 0},
-    {0, 0, 0, 0, 2, 1, 1, 1, 3, 0, 0, 0},
+    {0, 4, 0, 0, 2, 1, 1, 1, 3, 0, 0, 0},
     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 };
 
@@ -79,9 +79,33 @@ vector<Enemy> g_enemies;
 
 struct Bullet {
     int x, y;
-    int dx;
+    int dx, dy;
 };
 vector<Bullet> g_bullets;
+
+void ProcessKeyboardDown(WPARAM wParam);
+void ProcessKeyboardUp(WPARAM wParam);
+void DrawMap(HDC hdc);
+void InitPlayer();
+void MovePlayer();
+void DrawPlayer(HDC hDC);
+void ApplyGravity();
+bool IsColliding(int x, int y);
+bool IsSlopeGoRightColliding(int x, int y);
+bool IsSlopeGoLeftColliding(int x, int y);
+void GenerateItem(int x, int y, int num);
+void DrawItems(HDC hdc);
+void GenerateEnemy(int x, int y);
+void DrawEnemies(HDC hDC);
+
+void ShootBullet();
+void MoveBullets();
+void DrawBullets(HDC hDC);
+
+void CheckCollisions();
+void CheckEnemyPlayerCollisions();
+void CheckItemPlayerCollisions();
+void CheckPlayerBulletCollisions();
 
 // WinMain 함수
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow) {
@@ -116,7 +140,101 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdPa
     return Message.wParam;
 }
 
-// 함수
+//메인 함수
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+    PAINTSTRUCT ps;
+    HDC hDC = BeginPaint(hWnd, &ps);
+    HDC mDC;
+    HBITMAP hBitmap;
+    RECT rt;
+
+    static int shootInterval = 0;
+
+    switch (message) {
+    case WM_CREATE:
+        InitPlayer();
+        SetTimer(hWnd, 1, 1000 / 60, NULL);
+        break;
+    case WM_COMMAND:
+        switch (LOWORD(wParam)) {}
+        break;
+    case WM_CHAR:
+        switch (wParam)
+        {
+        case 'Q':
+        case 'q':
+            PostQuitMessage(0);
+            break;
+        }
+        break;
+    case WM_TIMER:
+        switch (wParam) {
+        case 1:
+            ApplyGravity();
+            MovePlayer();
+            MoveBullets();
+            shootInterval++;
+            if (shootInterval > 120) {
+                ShootBullet();
+                shootInterval = 0;
+            }
+            break;
+        }
+        InvalidateRect(hWnd, NULL, FALSE);
+        break;
+    case WM_PAINT:
+    {
+        GetClientRect(hWnd, &rt);
+        mDC = CreateCompatibleDC(hDC);
+        hBitmap = CreateCompatibleBitmap(hDC, BOARD_WIDTH, BOARD_HEIGHT);
+        SelectObject(mDC, (HBITMAP)hBitmap);
+
+        //--- 모든 그리기를 메모리 DC에한다.
+        DrawMap(mDC);
+        DrawEnemies(mDC);
+        DrawBullets(mDC);
+        DrawPlayer(mDC);
+        // 메모리 DC에서 화면 DC로 그림을 복사
+        // #1 맵 전체를 그리기
+        // BitBlt(hDC, 0, 0, BOARD_WIDTH, BOARD_HEIGHT, mDC, 0, 0, SRCCOPY);
+
+        // #2 플레이어 주변의 영역을 윈도우 전체로 확대
+        int stretchWidth = rt.right;
+        int stretchHeight = rt.bottom;
+        int sourceWidth = WINDOW_WIDTH;
+        int sourceHeight = WINDOW_HEIGHT;
+        int sourceX = g_player.x - WINDOW_WIDTH / 2;
+        if (sourceX <= 0) { sourceX = 0; }
+        if (g_player.x + WINDOW_WIDTH / 2 >= WINDOW_WIDTH) { sourceX = WINDOW_WIDTH - sourceWidth; }
+        int sourceY = g_player.y - WINDOW_HEIGHT / 2;
+        if (sourceY - sourceHeight >= 0) { sourceY = sourceHeight + GRID; }
+        if (sourceY <= 0) { sourceY = 0; }
+
+
+        StretchBlt(hDC, 0, 0, stretchWidth, stretchHeight, mDC, sourceX, sourceY, sourceWidth, sourceHeight, SRCCOPY);
+        
+        DeleteDC(mDC);
+        DeleteObject(hBitmap);
+        EndPaint(hWnd, &ps);
+        break;
+    }
+    case WM_KEYDOWN:
+        ProcessKeyboardDown(wParam);
+        InvalidateRect(hWnd, NULL, FALSE);
+        break;
+    case WM_KEYUP:
+        ProcessKeyboardUp(wParam);
+        InvalidateRect(hWnd, NULL, FALSE);
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
 
 // 키입력
 void ProcessKeyboardDown(WPARAM wParam) {
@@ -136,7 +254,7 @@ void ProcessKeyboardDown(WPARAM wParam) {
             g_player.isCharging = true;
             g_player.dx = 0;
             g_player.jumpSpeed -= 5;
-        }        
+        }
         break;
     }
 }
@@ -156,6 +274,60 @@ void ProcessKeyboardUp(WPARAM wParam) {
     }
 }
 
+// 맵
+void DrawMap(HDC hdc) {
+    for (int y = 0; y < MAP_HEIGHT; y++) {
+        for (int x = 0; x < MAP_WIDTH; x++) {
+            // 현재 맵의 값이 1이면 흰색(플레이어 영역), 0이면 검은색으로 그립니다.
+            if (map0[y][x] == 0) {
+                // 검은색
+                HBRUSH hBrush = CreateSolidBrush(RGB(0, 0, 0));
+                SelectObject(hdc, hBrush);
+                Rectangle(hdc, x * GRID, y * GRID, (x + 1) * GRID, (y + 1) * GRID);
+                DeleteObject(hBrush);
+            }
+            else if (map0[y][x] == 1) {
+                // 흰색
+                HBRUSH hBrush = CreateSolidBrush(RGB(255, 255, 255));
+                SelectObject(hdc, hBrush);
+                Rectangle(hdc, x * GRID, y * GRID, (x + 1) * GRID, (y + 1) * GRID);
+                DeleteObject(hBrush);
+            }
+            else if (map0[y][x] == 2) {  // 오른쪽 아래로 흘러내리는 빗면
+                // 검은색
+                POINT point[3];
+                point[0].x = x * GRID;
+                point[0].y = y * GRID;
+                point[1].x = x * GRID;
+                point[1].y = (y + 1) * GRID;
+                point[2].x = (x + 1) * GRID;
+                point[2].y = (y + 1) * GRID;
+                HBRUSH hBrush = CreateSolidBrush(RGB(255, 0, 0));
+                SelectObject(hdc, hBrush);
+                Polygon(hdc, point, 3);
+                DeleteObject(hBrush);
+            }
+            else if (map0[y][x] == 3) {  // 왼쪽 아래로 흘러내리는 빗면
+                // 검은색
+                POINT point[3];
+                point[0].x = (x + 1) * GRID;
+                point[0].y = y * GRID;
+                point[1].x = x * GRID;
+                point[1].y = (y + 1) * GRID;
+                point[2].x = (x + 1) * GRID;
+                point[2].y = (y + 1) * GRID;
+                HBRUSH hBrush = CreateSolidBrush(RGB(255, 0, 0));
+                SelectObject(hdc, hBrush);
+                Polygon(hdc, point, 3);
+                DeleteObject(hBrush);
+            }
+            else if (map0[y][x] == 4) {  // 적
+                GenerateEnemy(x, y);
+            }
+        }
+    }
+}
+
 // 플레이어
 void InitPlayer() {
     g_player.x = (MAP_WIDTH - 7) * GRID;
@@ -164,6 +336,71 @@ void InitPlayer() {
     g_player.dy = 0;
     g_player.jumpSpeed = 0;
     g_player.isCharging = false;
+}
+
+void MovePlayer() {
+    int newX = g_player.x + g_player.dx;
+    int newY = g_player.y + g_player.dy;
+
+    bool isVerticalCollision = IsColliding(g_player.x, newY);
+    bool isHorizontalCollision = IsColliding(newX, g_player.y);
+
+    // 수직 충돌 처리
+    if (!isVerticalCollision) {
+        g_player.y = newY;
+    }
+    else {
+        // 바닥 충돌 시 y축 위치 보정
+        if (g_player.dy > 0) {
+            while (!IsColliding(g_player.x, g_player.y + 1)) {
+                g_player.y += 1;
+            }
+        }
+        g_player.dy = 0; // 충돌 후 y축 속도 초기화
+    }
+
+    // 수평 충돌 처리
+    if (!isHorizontalCollision) {
+        g_player.x = newX;
+    }
+    else {
+        g_player.dx = 0; // 충돌 후 x축 속도 초기화
+    }
+    bool isSlopeGoRighCollision = IsSlopeGoRightColliding(g_player.x, g_player.y);
+    if (isSlopeGoRighCollision) {
+        g_player.isSlooping = true;
+
+        g_player.dy = 1; // 경사면 위에서 미끄러짐 속도
+        g_player.dx = 2; // 오른쪽 아래로 미끄러짐
+        newX = g_player.x + g_player.dx;
+        newY = g_player.y + g_player.dy;
+        g_player.x = newX;
+        g_player.y = newY;
+    }
+    else {
+        g_player.isSlooping = false;
+    }
+    bool isSlopeGoLeftCollision = IsSlopeGoLeftColliding(g_player.x, g_player.y);
+    if (isSlopeGoLeftCollision) {
+        g_player.isSlooping = true;
+
+        g_player.dy = 1; // 경사면 위에서 미끄러짐 속도
+        g_player.dx = -2; // 오른쪽 아래로 미끄러짐
+        newX = g_player.x + g_player.dx;
+        newY = g_player.y + g_player.dy;
+        g_player.x = newX;
+        g_player.y = newY;
+    }
+    else {
+        g_player.isSlooping = false;
+    }
+}
+
+void DrawPlayer(HDC hdc) {
+    HBRUSH hBrush = CreateSolidBrush(RGB(255, 255, 255));
+    SelectObject(hdc, hBrush);
+    Rectangle(hdc, g_player.x - PLAYER_SIZE / 2, g_player.y - PLAYER_SIZE / 2, g_player.x + PLAYER_SIZE / 2, g_player.y + PLAYER_SIZE / 2);
+    DeleteObject(hBrush);
 }
 
 void ApplyGravity() {
@@ -220,110 +457,6 @@ bool IsSlopeGoLeftColliding(int x, int y) {
     return false;
 }
 
-void MovePlayer() {
-    int newX = g_player.x + g_player.dx;
-    int newY = g_player.y + g_player.dy;
-
-    bool isVerticalCollision = IsColliding(g_player.x, newY);
-    bool isHorizontalCollision = IsColliding(newX, g_player.y);
-    
-    // 수직 충돌 처리
-    if (!isVerticalCollision) {
-        g_player.y = newY;
-    }
-    else {
-        // 바닥 충돌 시 y축 위치 보정
-        if (g_player.dy > 0) {
-            while (!IsColliding(g_player.x, g_player.y + 1)) {
-                g_player.y += 1;
-            }
-        }
-        g_player.dy = 0; // 충돌 후 y축 속도 초기화
-    }
-
-    // 수평 충돌 처리
-    if (!isHorizontalCollision) {
-        g_player.x = newX;
-    }
-    else {
-        g_player.dx = 0; // 충돌 후 x축 속도 초기화
-    }
-    bool isSlopeGoRighCollision = IsSlopeGoRightColliding(g_player.x, g_player.y);
-    if (isSlopeGoRighCollision) {
-        g_player.isSlooping = true;
-        
-        g_player.dy = 1; // 경사면 위에서 미끄러짐 속도
-        g_player.dx = 2; // 오른쪽 아래로 미끄러짐
-        newX = g_player.x + g_player.dx;
-        newY = g_player.y + g_player.dy;
-        g_player.x = newX;
-        g_player.y = newY;
-    }
-    else {
-        g_player.isSlooping = false;
-    }
-    bool isSlopeGoLeftCollision = IsSlopeGoLeftColliding(g_player.x, g_player.y);
-    if (isSlopeGoLeftCollision) {
-        g_player.isSlooping = true;
-
-        g_player.dy = 1; // 경사면 위에서 미끄러짐 속도
-        g_player.dx = -2; // 오른쪽 아래로 미끄러짐
-        newX = g_player.x + g_player.dx;
-        newY = g_player.y + g_player.dy;
-        g_player.x = newX;
-        g_player.y = newY;
-    }
-    else {
-        g_player.isSlooping = false;
-    }
-}
-
-void DrawPlayer(HDC hdc) {
-    HBRUSH hBrush = CreateSolidBrush(RGB(255, 255, 255));
-    SelectObject(hdc, hBrush);
-    Rectangle(hdc, g_player.x - PLAYER_SIZE / 2, g_player.y - PLAYER_SIZE / 2, g_player.x + PLAYER_SIZE / 2, g_player.y + PLAYER_SIZE / 2);
-    DeleteObject(hBrush);
-}
-
-// 적
-void GenerateEnemy(int x, int y) {
-    Enemy newEnemy;
-    newEnemy.x = x * GRID;
-    newEnemy.y = y * GRID;
-    g_enemies.push_back(newEnemy);
-}
-
-void ShootBullet() {
-    //for (auto it = g_enemies.begin(); it != g_enemies.end();) {
-        Bullet newBullet;
-        newBullet.x = g_player.x;
-        newBullet.y = g_player.y;//it->y;
-        newBullet.dx = 1;
-        g_bullets.push_back(newBullet);
-   //}
-}
-
-void MoveBullets() {
-    for (auto it = g_bullets.begin(); it != g_bullets.end();) {
-        it->x += it->dx;
-        if (it->x < 0 || it->x > BOARD_WIDTH) {
-            it = g_bullets.erase(it);
-        }
-        else {
-            ++it;
-        }
-    }
-}
-
-void DrawBullets(HDC hdc) {
-    HBRUSH hBrush = CreateSolidBrush(RGB(255, 0, 0));
-    SelectObject(hdc, hBrush);
-    for (const auto& bullet : g_bullets) {
-        Rectangle(hdc, bullet.x * GRID, bullet.y * GRID, (bullet.x + 1) * GRID, (bullet.y + 1) * GRID);
-    }
-    DeleteObject(hBrush);
-}
-
 // 아이템
 void GenerateItem(int x, int y, int num) {
     Item newItem;
@@ -341,6 +474,59 @@ void DrawItems(HDC hdc) {
     }
 }
 
+// 적
+void GenerateEnemy(int x, int y) {
+    Enemy newEnemy;
+    newEnemy.x = x;
+    newEnemy.y = y;
+    g_enemies.push_back(newEnemy);
+}
+
+void DrawEnemies(HDC hdc) {
+    for (const auto& enemy : g_enemies) {
+        HBRUSH hBrush = CreateSolidBrush(RGB(255, 255, 0));
+        SelectObject(hdc, hBrush);
+        Rectangle(hdc, enemy.x * GRID, enemy.y * GRID, (enemy.x + 1) * GRID, (enemy.y + 1) * GRID);
+        DeleteObject(hBrush);
+    }
+}
+
+void ShootBullet() {
+    for (const auto& enemy : g_enemies) {
+        Bullet newBullet;
+        newBullet.x = enemy.x * GRID; // 적의 위치에서 총알이 나가도록 설정
+        newBullet.y = enemy.y * GRID + GRID / 2;
+        newBullet.dx = 2;
+        newBullet.dy = 0;
+        g_bullets.push_back(newBullet);
+    }
+}
+
+
+void MoveBullets() {
+    for (auto it = g_bullets.begin(); it != g_bullets.end();) {
+        it->x += it->dx;
+        it->y += it->dy;
+        if (it->x < 0 || it->x > BOARD_WIDTH) {
+            it = g_bullets.erase(it);
+        }
+        else {
+            ++it;
+        }
+    }
+}
+
+void DrawBullets(HDC hdc) {
+    HBRUSH hBrush = CreateSolidBrush(RGB(255, 255, 255));
+    SelectObject(hdc, hBrush);
+    for (const auto& bullet : g_bullets) {
+        if (bullet.x >= 0 && bullet.x <= BOARD_WIDTH && bullet.y >= 0 && bullet.y <= BOARD_HEIGHT) {
+            Ellipse(hdc, bullet.x - 10, bullet.y - 10, bullet.x + 10, bullet.y + 10);
+        }
+    }
+    DeleteObject(hBrush);
+}
+
 // 충돌 확인 함수
 void CheckItemPlayerCollisions() {
     for (auto it = g_items.begin(); it != g_items.end(); ) {
@@ -356,154 +542,4 @@ void CheckItemPlayerCollisions() {
 
 void CheckCollisions() {
     CheckItemPlayerCollisions();
-}
-
-void DrawMap(HDC hdc) {
-    for (int y = 0; y < MAP_HEIGHT; y++) {
-        for (int x = 0; x < MAP_WIDTH; x++) {
-            // 현재 맵의 값이 1이면 흰색(플레이어 영역), 0이면 검은색으로 그립니다.
-            if (map0[y][x] == 0) {
-                // 검은색
-                HBRUSH hBrush = CreateSolidBrush(RGB(0, 0, 0));
-                SelectObject(hdc, hBrush);
-                Rectangle(hdc, x * GRID, y * GRID, (x + 1) * GRID, (y + 1) * GRID);
-                DeleteObject(hBrush);
-            }
-            else if (map0[y][x] == 1) {
-                // 흰색
-                HBRUSH hBrush = CreateSolidBrush(RGB(255, 255, 255));
-                SelectObject(hdc, hBrush);
-                Rectangle(hdc, x * GRID, y * GRID, (x + 1) * GRID, (y + 1) * GRID);
-                DeleteObject(hBrush);
-            }
-            else if (map0[y][x] == 2) {  // 오른쪽 아래로 흘러내리는 빗면
-                // 검은색
-                POINT point[3];
-                point[0].x = x * GRID;
-                point[0].y = y * GRID;
-                point[1].x = x * GRID;
-                point[1].y = (y + 1) * GRID;
-                point[2].x = (x + 1) * GRID;
-                point[2].y = (y + 1) * GRID;
-                HBRUSH hBrush = CreateSolidBrush(RGB(255, 0, 0));
-                SelectObject(hdc, hBrush);
-                Polygon(hdc, point, 3);
-                DeleteObject(hBrush);
-            }
-            else if (map0[y][x] == 3) {  // 왼쪽 아래로 흘러내리는 빗면
-                // 검은색
-                POINT point[3];
-                point[0].x = (x + 1) * GRID;
-                point[0].y = y * GRID;
-                point[1].x = x * GRID;
-                point[1].y = (y + 1) * GRID;
-                point[2].x = (x + 1) * GRID;
-                point[2].y = (y + 1) * GRID;
-                HBRUSH hBrush = CreateSolidBrush(RGB(255, 0, 0));
-                SelectObject(hdc, hBrush);
-                Polygon(hdc, point, 3);
-                DeleteObject(hBrush);
-            }
-            else if (map0[y][x] == 4) {  // 적
-                GenerateEnemy(x, y);
-                HBRUSH hBrush = CreateSolidBrush(RGB(255, 255, 0));
-                SelectObject(hdc, hBrush);
-                Rectangle(hdc, x * GRID, y * GRID, (x + 1) * GRID, (y + 1) * GRID);
-                DeleteObject(hBrush);
-            }
-        }
-    }
-}
-
-//메인 함수
-
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
-    PAINTSTRUCT ps;
-    HDC hDC = BeginPaint(hWnd, &ps);
-    HDC mDC;
-    HBITMAP hBitmap;
-    RECT rt;
-
-    static int shootInterval = 0;
-
-    switch (message) {
-    case WM_CREATE:
-        InitPlayer();
-        SetTimer(hWnd, 1, 1000 / 60, NULL);
-        break;
-    case WM_COMMAND:
-        switch (LOWORD(wParam)) {}
-        break;
-    case WM_CHAR:
-        switch (wParam)
-        {
-        case 'Q':
-        case 'q':
-            PostQuitMessage(0);
-            break;
-        case 'a':
-            ShootBullet();
-            break;
-        }
-        break;
-    case WM_TIMER:
-        switch (wParam) {
-        case 1:
-            ApplyGravity();
-            MovePlayer();
-            MoveBullets();
-            break;
-        }
-        InvalidateRect(hWnd, NULL, FALSE);
-        break;
-    case WM_PAINT:
-    {
-        GetClientRect(hWnd, &rt);
-        mDC = CreateCompatibleDC(hDC);
-        hBitmap = CreateCompatibleBitmap(hDC, BOARD_WIDTH, BOARD_HEIGHT);
-        SelectObject(mDC, (HBITMAP)hBitmap);
-
-        //--- 모든 그리기를 메모리 DC에한다.
-        DrawMap(mDC);
-        DrawBullets(mDC);
-        DrawPlayer(mDC);
-        // 메모리 DC에서 화면 DC로 그림을 복사
-        // #1 맵 전체를 그리기
-        // BitBlt(hDC, 0, 0, BOARD_WIDTH, BOARD_HEIGHT, mDC, 0, 0, SRCCOPY);
-
-        // #2 플레이어 주변의 영역을 윈도우 전체로 확대
-        int stretchWidth = rt.right;
-        int stretchHeight = rt.bottom;
-        int sourceWidth = WINDOW_WIDTH;
-        int sourceHeight = WINDOW_HEIGHT;
-        int sourceX = g_player.x - WINDOW_WIDTH / 2;
-        if (sourceX <= 0) { sourceX = 0; }
-        if (g_player.x + WINDOW_WIDTH / 2 >= WINDOW_WIDTH) { sourceX = WINDOW_WIDTH - sourceWidth; }
-        int sourceY = g_player.y - WINDOW_HEIGHT / 2;
-        if (sourceY - sourceHeight >= 0) { sourceY = sourceHeight + GRID; }
-        if (sourceY <= 0) { sourceY = 0; }
-
-
-        StretchBlt(hDC, 0, 0, stretchWidth, stretchHeight, mDC, sourceX, sourceY, sourceWidth, sourceHeight, SRCCOPY);
-        
-        DeleteDC(mDC);
-        DeleteObject(hBitmap);
-        EndPaint(hWnd, &ps);
-        break;
-    }
-    case WM_KEYDOWN:
-        ProcessKeyboardDown(wParam);
-        InvalidateRect(hWnd, NULL, FALSE);
-        break;
-    case WM_KEYUP:
-        ProcessKeyboardUp(wParam);
-        InvalidateRect(hWnd, NULL, FALSE);
-        break;
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
-    default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
-    }
-    return 0;
 }
